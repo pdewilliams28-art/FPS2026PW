a57cd6fc92ccf8f79b24868b5d2f09da5ba73620
pdewilliams28-art <pdewilliams28@bfhsla.org>
2026-02-05 15:31:05 -0600

    Pain and damage

--- mobbie.gd
+++ mobbie.gd
@@ -1,6 +1,17 @@
 extends RigidBody3D
 
 @onready var bat_model: Node3D = $"bat_model (Inherited)"
+@export var health:= 5.0
+@onready var player = get_node("/root/World/Player")
 
-func take_damage():
+func take_damage(damage):
+	health -= damage
 	bat_model.hurt()
+	if health <= 0:
+		set_physics_process(false)
+		gravity_scale = 1.0
+		var direction = -global_position.direction_to(player.global_position)
+		var random_upward_force = Vector3.UP * randf() * 5
+		
+		apply_central_impulse(direction.rotated(Vector3.UP, randf_range(-0.2, 0.2)) * 10.0 * random_upward_force)
+	print(health)
--- scenes/projectile.gd
+++ scenes/projectile.gd
@@ -3,6 +3,7 @@ extends Area3D
 # expose use @export var
 const speed = 20.0
 const RANGE = 40.0
+@export var damage:= 1.0
 
 var travelled_distance = 0.0
 
@@ -13,3 +14,10 @@ func _physics_process(delta: float) -> void:
 	
 	if travelled_distance > RANGE:
 		queue_free()
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.has_method("take_damage"):
+		body.take_damage(damage)
+	#destroy bullet
+	queue_free()
